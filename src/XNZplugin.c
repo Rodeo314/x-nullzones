eab442fd9f363e224f44178337734c7a566f13d9
Tim Walker <tdskywalker@gmail.com>
2020-08-04 16:17:02 +0200

    WIP.

--- src/XNZplugin.c
+++ src/XNZplugin.c
@@ -21,13 +21,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "XPLM/XPLMDataAccess.h"
 #include "XPLM/XPLMPlanes.h"
 #include "XPLM/XPLMPlugin.h"
 #include "XPLM/XPLMUtilities.h"
 
-/* Miscellaneous data */
-int xplane_first_load = 1;
-void  *xnz_context = NULL;
+typedef struct
+{
+    XPLMDataRef f_air_speed;
+    XPLMDataRef f_grd_speed;
+    XPLMDataRef nullzone[3];
+    float prefs_nullzone[3];
+}
+xnz_context;
+
+xnz_context *context = NULL;
 
 int xnz_plugin_start(char *outName, char *outSig, char *outDesc)
 {
@@ -36,7 +44,7 @@ int xnz_plugin_start(char *outName, char *outSig, char *outDesc)
     strncpy(outDesc, "Dynamic nullzones and other miscellaneous stuff", 255);
 
     /* all good */
-    XPLMDebugString("navP [info]: xnz_plugin_start OK\n"); return 1;
+    XPLMDebugString("x-nullzones [info]: xnz_plugin_start OK\n"); return 1;
 }
 
 void xnz_plugin_stop(void)
@@ -47,58 +55,80 @@ void xnz_plugin_stop(void)
 int xnz_plugin_enable(void)
 {
     /* Initialize context */
-    //fixme
+    if (NULL == (context = malloc(sizeof(xnz_context))))
+    {
+        XPLMDebugString("x-nullzones [error]: xnz_plugin_enable failed (malloc)\n"); goto fail;
+    }
+    if (NULL == (context->nullzone[0] = XPLMFindDataRef("sim/joystick/joystick_pitch_nullzone")))
+    {
+        XPLMDebugString("x-nullzones [error]: xnz_plugin_enable failed (ref[0])\n"); goto fail;
+    }
+    if (NULL == (context->nullzone[1] = XPLMFindDataRef("sim/joystick/joystick_roll_nullzone")))
+    {
+        XPLMDebugString("x-nullzones [error]: xnz_plugin_enable failed (ref[1])\n"); goto fail;
+    }
+    if (NULL == (context->nullzone[2] = XPLMFindDataRef("sim/joystick/joystick_heading_nullzone")))
+    {
+        XPLMDebugString("x-nullzones [error]: xnz_plugin_enable failed (ref[2])\n"); goto fail;
+    }
 
     /* all good */
-    XPLMDebugString("navP [info]: xnz_plugin_enable OK\n"); return 1;
+    XPLMDebugString("x-nullzones [info]: xnz_plugin_enable OK\n");
+    return 1;
+
+fail:
+    if (NULL != context)
+    {
+        free(context);
+        context = NULL;
+    }
+    return 0;
 }
 
 void xnz_plugin_disable(void)
 {
+    /* reset nullzones to default/preferences values */
+    XPLMSetDataf(context->nullzone[0], context->prefs_nullzone[0]);
+    XPLMSetDataf(context->nullzone[1], context->prefs_nullzone[1]);
+    XPLMSetDataf(context->nullzone[2], context->prefs_nullzone[2]);
+
     /* close context */
-    if (xnz_context)
+    if (NULL != context)
     {
-        //fixme
+        free(context);
+        context = NULL;
     }
 
     /* all good */
-    XPLMDebugString("navP [info]: xnz_plugin_disable OK\n");
+    XPLMDebugString("x-nullzones [info]: xnz_plugin_disable OK\n");
 }
 
-void xnz_plugin_message(XPLMPluginID inFromWho,
-                        long         inMessage,
-                        void        *inParam)
+void xnz_plugin_message(XPLMPluginID inFromWho, long inMessage, void *inParam)
 {
     switch (inMessage)
     {
-        case XPLM_MSG_PLANE_CRASHED:
-            break;
-
         case XPLM_MSG_PLANE_LOADED:
-            if (xplane_first_load)
+            if (inParam == XPLM_USER_AIRCRAFT) // user's plane changing
             {
-                xplane_first_load = 0;
+                context->prefs_nullzone[0] = XPLMGetDataf(context->nullzone[0]);
+                context->prefs_nullzone[1] = XPLMGetDataf(context->nullzone[1]);
+                context->prefs_nullzone[2] = XPLMGetDataf(context->nullzone[2]);
             }
-            break;
-
-        case XPLM_MSG_AIRPORT_LOADED:
-            break;
-
-        case XPLM_MSG_SCENERY_LOADED:
-            break;
-
-        case XPLM_MSG_AIRPLANE_COUNT_CHANGED:
             break;
 
         case XPLM_MSG_PLANE_UNLOADED:
             if (inParam == XPLM_USER_AIRCRAFT) // user's plane changing
             {
-                //fixme
+                XPLMSetDataf(context->nullzone[0], context->prefs_nullzone[0]);
+                XPLMSetDataf(context->nullzone[1], context->prefs_nullzone[1]);
+                XPLMSetDataf(context->nullzone[2], context->prefs_nullzone[2]);
             }
             break;
 
         case XPLM_MSG_WILL_WRITE_PREFS:
-            //fixme
+            XPLMSetDataf(context->nullzone[0], context->prefs_nullzone[0]);
+            XPLMSetDataf(context->nullzone[1], context->prefs_nullzone[1]);
+            XPLMSetDataf(context->nullzone[2], context->prefs_nullzone[2]);
             break;
 
         case XPLM_MSG_LIVERY_LOADED:
